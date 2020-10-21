f0f13b505ebef1129224e5aa31a49618a9cab78c
Francis Luigie Quenano <flquenano@gmail.com>
2020-10-21 15:48:52 -0500

    Home/Welcome done.

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { useSpring, animated, config } from "react-spring";
+import { useInView } from "react-intersection-observer";
+import WelcomeImage from "./image-welcome-3x.jpg";
+import {
+  Wrapper,
+  Content,
+  Img,
+  Text,
+  HeadingH1L,
+  HeadingH2
+} from "./welcome.css.js";
+
+export const Welcome = () => {
+  const [showText, setShowText] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+  const { ref, inView } = useInView({
+    threshold: 0.8,
+    triggerOnce: true
+  });
+  const textSpring = useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? "translateX(0%)" : "translateX(-20%)"
+  });
+
+  const imgSpring = useSpring({
+    to: {
+      opacity: inView ? 1 : 0,
+      transform: inView ? "translateX(0%)" : "translateX(20%)"
+    },
+    from: {
+      opacity: 0
+    },
+    delay: 300
+  });
+  const contentSpring = useSpring({
+    to: {
+      opacity: inView ? 1 : 0,
+      transform: inView ? "translateY(0%)" : "translateY(20%)"
+    },
+    from: {
+      opacity: 0
+    },
+    delay: 500
+  });
+
+  useEffect(() => {
+    const imgQuery = window.matchMedia("(min-width: 1024px)");
+    const textQuery = window.matchMedia("(min-width: 740px)");
+
+    textQuery.addEventListener("change", textMediaQuery);
+    textMediaQuery(textQuery);
+
+    imgQuery.addEventListener("change", imgMediaQuery);
+    imgMediaQuery(imgQuery);
+  });
+
+  const textMediaQuery = ({ matches }) => {
+    setShowText(matches);
+  };
+
+  const imgMediaQuery = ({ matches }) => {
+    setShowImage(matches);
+  };
+
+  return (
+    <Wrapper ref={ref}>
+      {showImage ? (
+        <Img src={WelcomeImage} alt="Welcome" style={{ ...imgSpring }} />
+      ) : null}
+
+      {showText ? (
+        <animated.div style={textSpring}>
+          <HeadingH1L size="H1L" style={textSpring}>
+            Welcome
+          </HeadingH1L>
+        </animated.div>
+      ) : null}
+
+      <Content style={contentSpring}>
+        <HeadingH2 size="H2">Welcome to Arch Studio</HeadingH2>
+        <Text>
+          We have a unique network and skillset to help bring your projects to
+          life. Our small team of highly skilled individuals combined with our
+          large network put us in a strong position to deliver exceptional
+          results.
+        </Text>
+        <Text>
+          Over the past 10 years, we have worked on all kinds of projects. From
+          stations to high-rise buildings, we create spaces that inspire and
+          delight.
+        </Text>
+        <Text>
+          We work closely with our clients so that we understand the intricacies
+          of each project. This allows us to work in harmony the surrounding
+          area to create truly stunning projects that will stand the test of
+          time.
+        </Text>
+      </Content>
+    </Wrapper>
+  );
+};
---
+++ src/components/Welcome/welcome.css.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import { animated } from "react-spring";
+import { Heading as DefaultHeading } from "../Heading";
+import { Text as DefaultText } from "../Text";
+
+export const Wrapper = styled.div`
+  margin: 7.2rem auto 9.7rem;
+  position: relative;
+  width: 100%;
+  @media ${(props) => props.theme.breakpoints.md} {
+    max-width: 114rem;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 65px;
+    top: 0;
+    left: 8.5%;
+    border-top: 1px solid ${(props) => props.theme.colors.lightGrey};
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+`;
+
+export const Img = styled(animated.img)`
+  position: absolute;
+  top: 10rem;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+export const Content = styled(animated.div)`
+  position: relative;
+  padding: 6.7rem 8.5% 0;
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 0;
+    width: 100%;
+    max-width: 58rem;
+    top: -2.65rem;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    max-width: 44.6rem;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    width: 44.6rem;
+    left: 19rem;
+    top: 0;
+  }
+`;
+
+export const Text = styled(DefaultText)`
+  max-width: 57.2rem;
+  text-align: start;
+  &:not(:last-child) {
+    padding-bottom: 1.75rem;
+  }
+`;
+
+export const HeadingH1L = styled(DefaultHeading)`
+  position: relative;
+  @media ${(props) => props.theme.breakpoints.md} {
+    position: relative;
+    top: 0;
+    left: 0;
+    z-index: 5;
+  }
+`;
+export const HeadingH2 = styled(DefaultHeading)`
+  padding-bottom: 5.6rem;
+  max-width: 44.6rem;
+`;
